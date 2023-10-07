d4b1a16e60433e3acc3c005b71418b95a4fe4d75
vrnsky <voronyansky.egor@yandex.ru>
2023-10-07 18:05:13 +0800

    Initial commit with stub implementation

---
+++ src/main/java/my/edu/sunway/wbrms/wbrmsinventoryservice/WbrmsInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbrmsInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbrmsInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/sunway/wbrms/wbrmsinventoryservice/WbrmsInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbrmsInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
